000bee0138b22af02dbf4a7676e47e3401578240
Juncoder <ssytn2@nvaer.com>
2021-03-25 22:30:17 +0900

    fake Db

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.render("join", { pageTitle: "Join" });
+};
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "UserDetail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "EditProfile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "ChangePassword" });
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1212321,
+      name: "youngjun",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 324231233,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 12123121,
+      name: "youngjun",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 324393,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 112341,
+      name: "youngjun",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 12393,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121241121,
+      name: "youngjun",
+      email: "nico@las.com",
+    },
+  },
+];
