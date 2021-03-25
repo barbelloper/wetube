887dc0cb9836aba588d7232e6da6cd02bdb08202
Juncoder <ssytn2@nvaer.com>
2021-03-26 02:14:08 +0900

    modifiy controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,31 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.render("join", { pageTitle: "Join" });
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    //To Do: Register User
+    //To Do: Log user in
+    res.redirect(routes.home);
+  }
 };
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Logout" });
+export const getLogin = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+export const logout = (req, res) => {
+  //To Do: Process Log Out
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "UserDetail" });
 export const editProfile = (req, res) =>
